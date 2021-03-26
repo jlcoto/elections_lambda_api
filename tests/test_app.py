18814c919d51b4ebc42195943c3ceb0eb411d25a
Jose  Coto Zevallos <jose@Joses-MacBook-Air.local>
2021-03-26 14:20:09 +0100

    Add and reformat tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,7 @@ import pytest
 
 from app import app
 
+
 @pytest.fixture
 def client(mocker):
     app.config["PRIVATE_API_KEY"] = "valid-key"
@@ -9,12 +10,14 @@ def client(mocker):
     with app.app_context():
         yield client
 
+
 def test_hello(client):
-    response = client.get('/')
+    response = client.get("/")
     assert response.status_code == 200
-    assert response.data.decode() == 'Hello world!'
+    assert response.data.decode() == "Hello world!"
+
 
 def test_hello_name(client):
-    response = client.get('/hello/Jose')
+    response = client.get("/hello/Jose")
     assert response.status_code == 200
-    assert response.data.decode() == 'Hola Jose!'
+    assert response.data.decode() == "Hola Jose!"
--- tests/test_response_adapter.py
+++ tests/test_response_adapter.py
@@ -7,16 +7,21 @@ def test_adapt_party_data():
     data = {
         "Items": [
             {
-                "party": {
-                    "S": "El Frente Amplio Por Justicia, Vida Y Libertad"
-                },
+                "party": {"S": "Frente Amplio"},
                 "partyId": {"S": "00000013"},
                 "votes": {"N": "911700"},
             },
             {
-                "party": {"S": "Partido Politico Nacional Peru Libre"},
+                "party": {"S": "Peru Libre"},
                 "partyId": {"S": "00000017"},
                 "votes": {"N": "502898"},
             },
         ]
     }
+
+    expected_adapted_data = [
+        {"party": "Frente Amplio", "votes": 911700},
+        {"party": "Peru Libre", "votes": 502898},
+    ]
+
+    assert adapter.adapt_party_data(data) == expected_adapted_data
