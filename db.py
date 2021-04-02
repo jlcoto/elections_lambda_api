f49ba2d8803bde09b831fe89be28d0845ca4bd02
Jose  Coto Zevallos <jose@Joses-MacBook-Air.local>
2021-04-02 18:27:43 +0200

    Don't pass profile to boto

--- db.py
+++ db.py
@@ -5,9 +5,5 @@ TABLE_NAME = "electionResults"
 
 class DB:
     @property
-    def session(self):
-        return boto3.Session(profile_name="jose_default")
-
-    @property
     def client(self):
-        return self.session.client("dynamodb", region_name="eu-central-1")
+        return boto3.client("dynamodb", region_name="eu-central-1")
